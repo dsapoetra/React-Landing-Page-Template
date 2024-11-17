c4d9ac7a68cefea53882ed33a6797fd480cbc13f
dsapoetra <user@Users-MacBook-Pro.local>
2024-11-17 17:41:15 +0700

    Initial redesign

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -7,8 +7,7 @@ export const Services = (props) => {
         <div className="section-title">
           <h2>Our Services</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
+            Service I offer.
           </p>
         </div>
         <div className="row">
